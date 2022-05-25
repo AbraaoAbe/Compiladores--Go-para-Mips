7d83e3c57e9e2008db210d5483a2b3a3e5d6f318
Igor Varejão <imsvarejao@gmail.com>
2022-05-24 21:30:00 -0300

    bash script adicionado

---
+++ run.sh
@@ -0,0 +1,5 @@
+java -jar /usr/local/lib/antlr-4.10.1-complete.jar -no-listener -o src-parser GoLexer.g4 GoParser.g4
+javac -cp .:/usr/local/lib/antlr-4.10.1-complete.jar ParserBase/GoParserBase.java src-parser/*.java
+cp ParserBase/GoParserBase.class src-parser/
+cd src-parser
+java org.antlr.v4.gui.TestRig Go declaration ../tests/in/in.go -tokens
