f91db12b0806367c354fd9a8af1a5ada7612f887
Igor Varejão <imsvarejao@gmail.com>
2022-07-12 16:55:22 -0300

    del run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-rm -rf src-parser
-java -jar /usr/local/lib/antlr-4.10.1-complete.jar -no-listener -o src-parser GoLexer.g4 GoParser.g4
-javac -cp .:/usr/local/lib/antlr-4.10.1-complete.jar ParserBase/GoParserBase.java src-parser/*.java
-cp ParserBase/GoParserBase.class src-parser/
-cd src-parser
-java org.antlr.v4.gui.TestRig Go sourceFile ../tests/in/in.go -gui
